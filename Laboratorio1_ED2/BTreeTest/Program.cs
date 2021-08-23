19182a9b621cacc11d2c768cf6f12350443c5368
Ghandalf312 <djmoralesm@correo.url.edu.gt>
2021-08-23 17:24:40 -0600

    commit incial

---
+++ Laboratorio1_ED2/BTreeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTreeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
