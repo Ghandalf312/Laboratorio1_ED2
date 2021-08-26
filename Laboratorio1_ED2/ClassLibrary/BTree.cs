f49ff2da584cce03d4b4db048ca8fc9ab02a515b
Ghandalf312 <djmoralesm@correo.url.edu.gt>
2021-08-25 21:30:52 -0600

    Eliminacion hecha, pendiente los recorridos del arbol y y buscar en el nodo

--- Laboratorio1_ED2/ClassLibrary/BTree.cs
+++ Laboratorio1_ED2/ClassLibrary/BTree.cs
@@ -166,7 +166,289 @@ namespace ClassLibrary
             }
         }
 
+        public bool Delete(T val)
+        {
+            if (Root != 0)
+            {
+                int pos = SearchNode(Root, val);
+                if (pos != 0)
+                {
+                    Remove(ChargeNode(pos), val);
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        private int SearchNode(int pos, T val)
+        {
+            var node = ChargeNode(pos);
+            for (int i = 0; i < Degree - 1; i++)
+            {
+                if (node.Values[i] != null)
+                {
+                    if (val.CompareTo(node.Values[i]) == 0)
+                        return pos;
+                    else if (val.CompareTo(node.Values[i]) < 0 && node.Sons[i] != 0)
+                        return SearchNode(node.Sons[i], val);
+                }
+                else if (node.Sons[i] != 0)
+                    return SearchNode(node.Sons[i], val);
+            }
+            if (node.Sons[Degree - 1] != 0)
+                return SearchNode(node.Sons[Degree - 1], val);
+            return 0;
+        }
+        private void Remove(BNode<T> node, T val)
+        {
+            bool deleted = false;
+            int originalPosition = 0;
+            for (int i = 0; i < Degree - 1; i++)
+            {
+                if (node.Values[i] != null)
+                {
+                    if (deleted && node.IsLeaf())
+                    {
+                        node.Values[i - 1] = node.Values[i];
+                        node.Values[i] = default;
+                    }
+                    if (node.Values[i] != null)
+                    {
+                        if (val.CompareTo(node.Values[i]) == 0)
+                        {
+                            originalPosition = i;
+                            node.Values[i] = default;
+                            deleted = true;
+                        }
+                    }
+                }
+            }
+            if (node.IsLeaf())
+            {
+                if (node.IsInUnderflow() && node.ID != Root)
+                    Rearrange(node);
+                else
+                    Write(node.ToFixedString(), node.ID);
+            }
+            else
+            {
+                var aux = ChargeNode(node.Sons[originalPosition + 1]);
+                while (!aux.IsLeaf())
+                {
+                    aux = ChargeNode(aux.Sons[0]);
+                }
+                node.Values[originalPosition] = aux.Values[0];
+                Write(node.ToFixedString(), node.ID);
+                Remove(aux, aux.Values[0]);
+            }
+        }
+        private void Rearrange(BNode<T> node)
+        {
+            var father = ChargeNode(node.Father);
+            int leftBrotherID = 0;
+            int rightBrotherID = 0;
+            for (int i = 0; i < Degree; i++)
+            {
+                if (father.Sons[i] == node.ID)
+                {
+                    if (i > 0)
+                        leftBrotherID = father.Sons[i - 1];
+                    if (i < Degree - 1)
+                        rightBrotherID = father.Sons[i + 1];
+                }
+            }
+            if (rightBrotherID != 0)
+            {
+                var brother = ChargeNode(rightBrotherID);
+                if (brother.CanLend())
+                    LendFromBrother(node, brother, father, true);
+                else
+                {
+                    if (leftBrotherID != 0)
+                    {
+                        var leftBrother = ChargeNode(leftBrotherID);
+                        if (leftBrother.CanLend())
+                            LendFromBrother(node, leftBrother, father, false);
+                        else
+                            JoinNodes(node, brother, father);
+                    }
+                    else
+                        JoinNodes(node, brother, father);
+                }
+            }
+            else
+            {
+                var brother = ChargeNode(leftBrotherID);
+                if (brother.CanLend())
+                    LendFromBrother(node, brother, father, false);
+                else
+                    JoinNodes(brother, node, father);
+            }
+        }
+        private void LendFromBrother(BNode<T> node, BNode<T> brother, BNode<T> father, bool fromRight)
+        {
+            if (fromRight)
+            {
+                for (int i = 0; i < Degree - 1; i++)
+                {
+                    if (father.Sons[i] == node.ID)
+                    {
+                        for (int j = 0; j < Degree - 1; j++)
+                        {
+                            if (node.Values[j] == null)
+                            {
+                                node.Values[j] = father.Values[i];
+                                father.Values[i] = brother.Values[0];
+                                node.Sons[j + 1] = brother.Sons[0];
+                                brother.Sons[0] = brother.Sons[1];
+                                for (int k = 1; k < Degree - 1; k++)
+                                {
+                                    if (brother.Values[k] != null)
+                                    {
+                                        brother.Values[k - 1] = brother.Values[k];
+                                        brother.Sons[k] = brother.Sons[k + 1];
+                                        brother.Values[k] = default;
+                                        brother.Sons[k + 1] = 0;
+                                    }
+                                    else
+                                        break;
+                                }
+                                Write(node.ToFixedString(), node.ID);
+                                Write(father.ToFixedString(), father.ID);
+                                Write(brother.ToFixedString(), brother.ID);
+                                if (node.Sons[j + 1] != 0)
+                                {
+                                    var son = ChargeNode(node.Sons[j + 1]);
+                                    son.Father = node.ID;
+                                    Write(son.ToFixedString(), son.ID);
+                                }
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Degree - 1; i++)
+                {
+                    if (father.Sons[i] == brother.ID)
+                    {
+                        for (int j = 0; j < Degree - 1; j++)
+                        {
+                            if (brother.Values[j] == null || j == Degree - 2)
+                            {
+                                if (brother.Values[j] == null)
+                                    j--;
+                                for (int k = Degree - 2; k > 0; k--)
+                                {
+                                    if (node.Values[k - 1] != null)
+                                    {
+                                        node.Values[k] = node.Values[k - 1];
+                                        node.Sons[k + 1] = node.Sons[k];
+                                    }
+                                }
+                                node.Sons[1] = node.Sons[0];
+                                node.Values[0] = father.Values[i];
+                                father.Values[i] = brother.Values[j];
+                                node.Sons[0] = brother.Sons[j + 1];
+                                brother.Values[j] = default;
+                                brother.Sons[j + 1] = 0;
+                                Write(node.ToFixedString(), node.ID);
+                                Write(father.ToFixedString(), father.ID);
+                                Write(brother.ToFixedString(), brother.ID);
+                                if (node.Sons[0] != 0)
+                                {
+                                    var son = ChargeNode(node.Sons[0]);
+                                    son.Father = node.ID;
+                                    Write(son.ToFixedString(), son.ID);
+                                }
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
 
+        private void JoinNodes(BNode<T> left, BNode<T> right, BNode<T> father)
+        {
+            for (int i = 0; i < Degree - 1; i++)
+            {
+                if (left.Values[i] == null)
+                {
+                    for (int j = 0; j < Degree - 1; j++)
+                    {
+                        if (father.Sons[j] == left.ID)
+                        {
+                            left.Values[i] = father.Values[j];
+                            father.Values[j] = default;
+                            father.Sons[j + 1] = 0;
+                            for (int k = j + 1; k < Degree - 1; k++)
+                            {
+                                if (father.Values[k] != null)
+                                {
+                                    father.Values[k - 1] = father.Values[k];
+                                    father.Sons[k] = father.Sons[k + 1];
+                                    father.Values[k] = default;
+                                    father.Sons[k + 1] = 0;
+                                }
+                                else
+                                    break;
+                            }
+                            break;
+                        }
+                    }
+                    i++;
+                    left.Sons[i] = right.Sons[0];
+                    right.Sons[0] = 0;
+                    if (left.Sons[i] != 0)
+                    {
+                        var son = ChargeNode(left.Sons[i]);
+                        son.Father = left.ID;
+                        Write(son.ToFixedString(), son.ID);
+                    }
+                    for (int j = 0; j < Degree - 1; j++)
+                    {
+                        if (right.Values[j] != null)
+                        {
+                            left.Values[i + j] = right.Values[j];
+                            left.Sons[i + j + 1] = right.Sons[j + 1];
+                            right.Values[j] = default;
+                            right.Sons[j + 1] = 0;
+                            if (left.Sons[i + j + 1] != 0)
+                            {
+                                var son1 = ChargeNode(left.Sons[i + j + 1]);
+                                son1.Father = left.ID;
+                                Write(son1.ToFixedString(), son1.ID);
+                            }
+                        }
+                        else
+                            break;
+                    }
+                    right.Father = 0;
+                    Write(left.ToFixedString(), left.ID);
+                    Write(right.ToFixedString(), right.ID);
+                    Write(father.ToFixedString(), father.ID);
+                    if (father.IsInUnderflow() && father.ID != Root)
+                        Rearrange(father);
+                    else if (father.IsEmpty())
+                    {
+                        Root = left.ID;
+                        left.Father = 0;
+                        father.Sons[0] = 0;
+                        Write($"{Degree}|{Root}|{NextPosition}", 0);
+                        Write(left.ToFixedString(), left.ID);
+                        Write(father.ToFixedString(), father.ID);
+                    }
+                    break;
+                }
+            }
+        }
         private BNode<T> ChargeNode(int id)
         {
             var file = new FileStream(Path, FileMode.OpenOrCreate);
